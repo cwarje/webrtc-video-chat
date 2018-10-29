2295cc1931076c96236e685aed16b1c9e97be40d
Cameron Warje <cwarje@gmail.com>
2018-10-29 01:05:17 -0700

    Cleanup

--- user.js
+++ user.js
@@ -3,6 +3,7 @@ module.exports = User
 // pass an id later, and name.
 // Creates a user with the specified attributes.
 function User (data) {
+    console.log('user created');
     data = data || {}
     // this.id = data.id || generateGUID()
     this.name = data.name || 'Anonymous'
@@ -13,14 +14,6 @@ function User (data) {
         position: 'relative'
     })
     document.body.appendChild(this.element)
-}
-
-// Generates a mock GUID, from https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
-function generateGUID() {
-    var S4 = function () {
-        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-    };
-    return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
 }
 
 User.prototype.addStream = function (stream) {
